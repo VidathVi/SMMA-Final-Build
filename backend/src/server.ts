d3ce380de077fd6e576e7de3b39c614a6ba3d030
Imashi Sanjana Amarathunga <imashi.20232211@iit.ac.lk>
2026-03-19 19:05:11 +0530

    modify the files
    - added findById, getAll, updateRole, updateProfile, deleteById
    - fixed bcrypt to bcryptjs
    -registered /api/users routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth"; 
 import assetRoutes from "./routes/asset";
+import userRoutes from "./routes/user";
 import pool from "./db/db";
 
 
@@ -25,6 +26,7 @@ app.get("/ping", (req, res) => {
 
 app.use("/api/auth", authRoutes);
 app.use("/api/assets", assetRoutes);
+app.use("/api/users", userRoutes);
 
 app.get("/api/health", async (req: Request, res: Response) => {
   try {
