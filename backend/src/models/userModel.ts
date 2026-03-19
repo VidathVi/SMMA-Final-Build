ad6e32dd7ffe0db214ba8bf7c058145f73de20db
Imashi Sanjana Amarathunga <imashi.20232211@iit.ac.lk>
2026-03-19 10:32:42 +0530

    add user model
     add user model with create and find by email functions

---
+++ backend/src/models/userModel.ts
@@ -0,0 +1,36 @@
+import pool from "../db/db";
+
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+  password: string;
+  role: string;
+}
+
+// create user
+export const createUser = async (
+  name: string,
+  email: string,
+  password: string,
+  role: string
+): Promise<User> => {
+  const result = await pool.query(
+    "INSERT INTO users (name, email, password, role) VALUES ($1, $2, $3, $4) RETURNING *",
+    [name, email, password, role]
+  );
+
+  return result.rows[0];
+};
+
+// get user by email
+export const findUserByEmail = async (
+  email: string
+): Promise<User | undefined> => {
+  const result = await pool.query(
+    "SELECT * FROM users WHERE email = $1",
+    [email]
+  );
+
+  return result.rows[0];
+};
