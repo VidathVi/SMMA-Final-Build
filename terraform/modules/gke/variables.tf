fbea5bebc0f7aa71786e7eb6811a6e66c7aca2bb
VidathVi <dassanayakevidath@gmail.com>
2026-07-03 16:05:51 +0530

    stage 5.10

---
+++ terraform/modules/gke/variables.tf
@@ -0,0 +1,33 @@
+variable "cluster_name" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "machine_type" {
+  type = string
+}
+
+variable "node_count" {
+  type = number
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "subnet_id" {
+  type = string
+}
+
+variable "pod_range_name" {
+  type = string
+}
+
+variable "service_range_name" {
+  type = string
+}
+
+
